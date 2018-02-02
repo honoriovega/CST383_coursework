89f6c1d459253bb800e3b4364dc30b0e815ab19a
Honorio Vega <hvega@csumb.edu>
2018-02-02 00:37:21 -0800

    finished file I/O and packages lab

--- r5-lab.R
+++ r5-lab.R
@@ -1,29 +1,42 @@
+#Author: Honorio Vega
+#Date: 02/01/2018
+# CST383 - Data Science - Lab: R I/O and packages
+
+
+# 3. Read the census summary data as data frame ‘dat’
 dat = read.csv("https://raw.githubusercontent.com/grbruns/cst383/master/1994-census-summary.csv")
 
-# View the first few lines of dat
+# 4.Look at the first lines of the data frame
 print(head(dat))
 
-# print the number of rows 
+# 5. How many rows in the data frame?
 print(nrow(dat))
 
-# Print the unique values in marital statues column
+# 6. What are all the distinct values found in column “marital_status”?
 print(unique(dat$marital_status))
 
-# print the amount of unique countrys in the country column
+# 7. How many distinct values are found in column “native_country”?
 print(length(unique(dat$native_country)))
 
 
-# 8 What are the min and max age values found in column “age”?
+# 8. What are the min and max age values found in column “age”?
 allAges = dat$age
-
 print( paste("Min age", min(allAges), "Max age", max(allAges)))
 
-# print a subset of data , a random sample of 500 rows
+# 9.Create a data frame ‘dat1’ by randomly sampling 500 rows of the data frame (without replacement).
 dat1 = dat[sample(1:nrow(dat),500),]
 
-# print the number of rows in the random sample, should be 500
-print(nrow(dat1))
+# 10. Check to make sure ‘dat1’ has 500 rows
+paste("dat1 length is 500:",nrow(dat1) == 500)
 
-print(dat1)
-#write.csv(dat1,"census-sample.csv")
+# 11. Write data frame ‘dat1’ to a CSV file named ‘census-sample.csv’.
+write.csv(dat1,"census-sample.csv")
 
+# 12. Save the data frame ‘dat’ to an R binary file named ‘census-binary.RData’.
+save(dat,file="census-binary.RData")
+
+# 13. Clear all objects from your R environment.
+rm(list = ls())
+
+#14. Load the ‘dat’ object from file ‘census-binary.RData’.  Did it appear to load faster than when you read the data from a CSV file? YES
+load("census-binary.RData")
