f3a3a2b0611a6477a6a6e96435c025ba5ea38972
Honorio Vega <hvega@csumb.edu>
2018-02-06 21:50:18 -0800

    Added homework 2 template

---
+++ Homework/hw2.R
@@ -0,0 +1,99 @@
+#@ 1 
+# assign the value of variable z to variable x
+
+x = z
+
+#@ 2 
+# assign "hello" to variable msg
+
+#@ 3 
+# check whether variable msg is numeric
+
+#@ 4 check whether variable b is logical
+
+#@ 5 assign the result of squaring x to variable x2
+
+#@ 6 
+# compute the length of the string obtained by appending strings 
+# s1 and s2, with a space between them
+
+#@ 7 compute the substring consisting of the 2nd-4th characters of string s
+
+#@ 8 assign the result of testing whether x is greater than 5 to variable xt
+
+#@ 9 check if variable x is NA
+
+#@ 10 compute the length of vector x
+
+#@ 11 create a vector foo from strings s1, s2, and s3
+
+#@ 12 get the first element of vector x
+
+#@ 13 create a vector x consisting of the integers from 50 to 100, in that order
+
+#@ 14 compute the mean value of numeric vector x
+
+#@ 15 create a vector x consisting of values 10, 20, 30, ..., 400
+
+#@ 16 
+# compute a vector consisting of the 3rd and 4th elements of a vector x
+
+#@ 17 
+# compute a vector consisting of the elements of a vector x that are negative
+
+#@ 18 
+# given two numeric vectors x and y of the same length, get a vector 
+# by adding the corresponding elements of x and y
+
+#@ 19 
+# given a vector x of strings, compute a vector that is like x but 
+# with "baz" added to the end
+
+#@ 20 
+# given a numeric vector x, compute a vector like x but sorted 
+# and with no duplicates (hint: R has a 'sort' function)
+
+#@ 21 
+# compute a data frame by getting rows 3, 5, and 7 from the 'faithful' data
+# frame that is built into R
+
+#@ 22
+# compute a data frame by taking rows 1-10 of the mtcars data frame,
+# and using only columns "mpg", "disp", and "qsec"
+
+#@ 23
+# compute the average horsepower across all cars in the mtcars data
+# (column "hp" gives the horsepower of a car)
+
+#@ 23
+# compute a vector that is like the "hp" column of mt cars, except
+# that it gives the relative horsepower, by dividng the "hp" value
+# by the average horsepower across all cars
+
+#@ 24
+# In one of the first lectures of class, we looked at creating a
+# system to identify the part # of a transmission part, based on
+# width and length measurements ("features") of the part.  Create
+# data frame named tr.dat containing the following training examples
+# for the system.  
+# part1: 3.1, 5.2
+# part1: 3.0, 5.3
+# part1: 3.0, 5.2
+# part2: 3.1, 4.8
+# part2: 3.2, 4.9
+# part2: 3.2, 4.8
+# part3: 3.4, 5.6
+# part3: 3.4, 5.5
+# part3: 3.3, 5.4
+# Each row above is a training example. In the data frame tr.dat
+# that you create, the column names should be "width", "len",
+# and "label".  The rows of the data frame should be in the order
+# shown above.  For the label values, use numbers 1, 2, or 3.
+
+#@ 25
+# x and y and numeric vectors of length 3.  Compute a vector d
+# such that d[1] is the (Euclidean) distance from x[1],y[1] to x[1],y[1],
+# d[2] is the distance from x[1],y[1] to x[2],y[2], and d[3] is the
+# distance from x[1],y[1] to x[3],y[3].
+
+
